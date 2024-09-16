90671ea491b7d287ba95154fa7de47869d313569
jamesL103 <jameskl7337@gmail.com>
2024-09-15 23:55:14 -0400

    Added proper game generation on GUI

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -5,6 +5,8 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**A class that creates and instantiates the gui for the game Minesweeper.
  *
@@ -12,7 +14,7 @@ import java.awt.event.ActionListener;
 public class MineSweeper  {
 
 
-
+    private static GameComponent game;
     /** Creates the surrounding gui to store the minesweeper game in.
      *
      */
@@ -26,15 +28,27 @@ public class MineSweeper  {
         };
         topBar.setLayout(new FlowLayout());
         topBar.add(new Label("Height"));
-        TextField heightInput = new TextField();
+        TextField heightInput = new TextField("20");
         topBar.add(heightInput);
         topBar.add(new Label("Width"));
-        TextField widthInput = new TextField();
+        TextField widthInput = new TextField("20");
         topBar.add(widthInput);
         topBar.add(new Label("Density"));
-        topBar.add(new TextField());
-        JButton generator = getGeneratorButton(heightInput, widthInput, container);
+        TextField densityInput = new TextField("0.3");
+        topBar.add(densityInput);
+
+        //list of all text inputs for parameters for the game
+        ArrayList<TextField> gameInputs = new ArrayList<>();
+        gameInputs.add(heightInput);
+        gameInputs.add(widthInput);
+        gameInputs.add(densityInput);
+
+        //button to generate a new game
+        JButton generator = getGeneratorButton(gameInputs, container);
         topBar.add(generator);
+
+
+
 
         container.add(topBar, BorderLayout.NORTH);
 
@@ -45,17 +59,23 @@ public class MineSweeper  {
         container.setVisible(true);
     }
 
-    private static JButton getGeneratorButton(TextField heightInput, TextField widthInput, JFrame container) {
+    private static JButton getGeneratorButton(ArrayList<TextField> inputs, JFrame container) {
         JButton generator = new JButton("Generate new Game");
         generator.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 //                generateNewGame(heightInput.getText(), widthInput.getText());
                 try {
-                    int height = Integer.parseInt(heightInput.getText());
-                    int width = Integer.parseInt(widthInput.getText());
-                    container.add(new GameComponent(height, width, 0.5), BorderLayout.CENTER);
+                    if (game != null) {
+                        container.remove(game);
+                    }
+                    int height = Integer.parseInt(inputs.get(0).getText());
+                    int width = Integer.parseInt(inputs.get(1).getText());
+                    float density = Float.parseFloat(inputs.get(2).getText());
+                    game = new GameComponent(height, width, density);
+                    container.add(game, BorderLayout.CENTER);
                     container.repaint();
+                    container.paintComponents(container.getGraphics());
 //                    System.out.println("height input text: " + heightInput.getText());
                 } catch (NumberFormatException ignored){
 
